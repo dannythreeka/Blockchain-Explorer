8bfb8a1f9133a67d26a618d14038a3b077577e60
dannythreeka <dannythreekai@gmail.com>
2025-04-15 00:07:42 +0800

    feat: set working mode

---
+++ app/contracts/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+export default function ContractsPage() {
+  return (
+    <Box p={4} textAlign="center">
+      <Typography variant="h4" gutterBottom>
+        Contracts Page
+      </Typography>
+      <Typography variant="body1" color="textSecondary">
+        This page is currently under construction. Please check back later.
+      </Typography>
+    </Box>
+  );
+}
---
+++ app/events/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+export default function EventsPage() {
+  return (
+    <Box p={4} textAlign="center">
+      <Typography variant="h4" gutterBottom>
+        Events Page
+      </Typography>
+      <Typography variant="body1" color="textSecondary">
+        This page is currently under construction. Please check back later.
+      </Typography>
+    </Box>
+  );
+}
